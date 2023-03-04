1a254e379ec8d4504035616f81764a448e94f662
Kevin <kevin.daniell552@gmail.com>
2023-03-04 17:09:02 -0500

    PAE: I added a new code where the program do four operations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
-public class Main {
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+        public class Main {
     public static void main(String[] args) {
+
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What's the operation you want to realize? \n \n  Write a number \n \n 1. Adittion \n 2. Subtract \n 3. Multiplication \n 4. Division" );
+        int operation = sc.nextInt();
+
+        System.out.println("First value");
+        double Num1 = sc.nextInt();
+        System.out.println("Second value");
+        double Num2 = sc.nextInt();
+
+        if (operation == 1){
+            System.out.println(Num1 + Num2);
+        }
+            else if (operation == 2){
+            System.out.println(Num1 - Num2);
+        }
+                else if (operation == 3){
+            System.out.println(Num1 * Num2);
+        }
+                    else if(operation == 4){
+            System.out.println(Num1 / Num2);
+        }
+
 
          }
     }
