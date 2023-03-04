4a75ff2b490666e71db5dc2358f79155831c12d1
Kevin <kevin.daniell552@gmail.com>
2023-03-04 15:40:50 -0500

    PAE: Modified src main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        967 = a  // 967 - 673 - 379 - 294 - 209 - 124 - 85 - 46 - 39 - 7 - 5 - 2 - 1 - 1
+        673 = b
+        c = (a - b)
+                for (i = 0; i < 12, í++{
+                    System.out.println(c);
+
+                }
+         }
     }
-}
