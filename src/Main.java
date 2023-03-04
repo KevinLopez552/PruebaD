4b72dffee88dca5bbc10e592417d2ce47b2c2a11
Daniel Garcia <danielgarciaabello@gmail.com>
2023-03-04 16:02:11 -0500

    PAE: hello world

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        967 = a  // 967 - 673 - 379 - 294 - 209 - 124 - 85 - 46 - 39 - 7 - 5 - 2 - 1 - 1
-        673 = b
-        c = (a - b)
-                for (i = 0; i < 12, í++{
-                    System.out.println(c);
-
-                }
+        System.out.println("hello world");
          }
     }
